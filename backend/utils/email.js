8c3b9fcce40b54b4d3047be2ab50cbcce7210b23
TatsumiBernal <147santiago1@gmail.com>
2025-06-26 16:53:00 -0500

    Email 1/3

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,7 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465, // o 587
+  secure: true, // true para 465, false para 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
